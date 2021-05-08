d19800ff426a4f3fd8dbe169f9ac512a43eed6fd
Tora Sirkka <tora@torasirkka.fi>
2021-05-08 15:12:06 -0700

    Added subclasses 'Quiz' and 'StudentQuiz'.

--- oo.py
+++ oo.py
@@ -31,11 +31,13 @@ class Exam:
     def __init__(self, name):
         """Initialize exam"""
         self.name = name
-
-    questions = []
+        self.questions = []
 
     def add_question(self, question):
         self.questions.append(question)
+
+    def calc_score(self, lst):
+        return sum(lst) / len(lst)
 
     def administer(self):
         """administer all questions in test & evaluate score."""
@@ -46,7 +48,28 @@ class Exam:
                 lst.append(1)
             else:
                 lst.append(0)
-        return sum(lst) / len(lst)
+        return self.calc_score(lst)
+
+
+class Quiz(Exam):
+    """Define Quiz class.
+
+    This class behaves like it's super class 'Exam', with the exception that
+    the administer-method should return 1 if the score is > 0.5 and false
+    otherwise."""
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def add_question(self, question):
+        super().add_question(question)
+
+    def calc_score(self, lst):
+        return round(sum(lst) / len(lst))
+
+    def administer(self):
+        result = super().administer()
+        return result
 
 
 class StudentExam:
@@ -56,30 +79,72 @@ class StudentExam:
         """Instantiate an exam for a student"""
         self.student = student
         self.exam = exam
+        self.score = None
+
+    def _print_score(self, score):
+        print(f"Your score is: {score}.")
 
     def take_test(self):
         print(f"\nHi, {self.student.first_name}. Welcome to the {self.exam.name} exam!")
         print("*" * 40)
         print("")
-        score = self.exam.administer()
-        print(f"Your score is: {score}.")
+        self.score = self.exam.administer()
+        self._print_score(self.score)
+        return self.score
+
+
+class StudentQuiz(StudentExam):
+    """Creating a student class."""
+
+    def __init__(self, student, exam):
+        super().__init__(student, exam)
+
+    def print_score(self, score):
+        print(f"Your quizz result is: {score}.")
+
+    def take_test(self):
+        self.score = super().take_test()
 
 
 if __name__ == "__main__":
 
-    # Create exam 'Midterm'
+    # For examining Exam and Quiz- related classes:
+    # Create question objects
+    alberta_capital = Question("What is the capital of Alberta?", "Edmonton")
+    python_author = Question("Who is the author of Python?", "Guido Van Rossum")
+    set_q = Question("What is the method for adding and element to a set?", ".add()")
+
+    # --------------------------------------------------------------
+    # Create instance of exam
     midterm = Exam("Midterm")
+
+    # Add questions to exam instance object 'midterm'
+    midterm.add_question(alberta_capital)
+    midterm.add_question(python_author)
+    midterm.add_question(set_q)
+
+    # Instantiate student 'Jasmine'
+    Jasmine = Student("Jasmine", "Debugger", "0101 Computer Street")
+
+    # Instantiate studentexam object,
+    studentexam = StudentExam(Jasmine, midterm)
+    studentexam.take_test()
+
+    # ---------------------------------------------------------------
+
+    # Create exam 'Quiz'
+    quiz1 = Quiz("Quiz")
 
     # Create question objects
     alberta_capital = Question("What is the capital of Alberta?", "Edmonton")
     python_author = Question("Who is the author of Python?", "Guido Van Rossum")
     set_q = Question("What is the method for adding and element to a set?", ".add()")
     # Add questions to Midterm exam
-    midterm.add_question(alberta_capital)
-    midterm.add_question(python_author)
-    midterm.add_question(set_q)
+    quiz1.add_question(alberta_capital)
+    quiz1.add_question(python_author)
+    quiz1.add_question(set_q)
 
     Jasmine = Student("Jasmine", "Debugger", "0101 Computer Street")
 
-    studentexam = StudentExam(Jasmine, midterm)
-    studentexam.take_test()
+    studentquiz = StudentQuiz(Jasmine, quiz1)
+    studentquiz.take_test()
